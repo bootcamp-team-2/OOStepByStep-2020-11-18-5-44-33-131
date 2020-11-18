15921ffb792dee2603f362d7cd31c609189361c0
Lin Liu <LLiu41@slb.com>
2020-11-18 18:58:48 +0800

    refactor: move tests code to proper place

--- OOStepByStepTest/StudentTest.cs
+++ OOStepByStepTest/StudentTest.cs
@@ -21,5 +21,38 @@ namespace OOStepByStepTest
             //then
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Should_Return_Message_With_Class_Number_When_Introduce()
+        {
+            //given
+            string expected = "My name is Tom. I am 18 years old. I am a student of class 2.";
+            Student student = new Student("Tom", 18, 2);
+
+            //when
+            string actual = student.Introduce();
+
+            //then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Message_With_Class_Number_When_Introduce_After_Enroll()
+        {
+            //given
+            string expected = "My name is Tom. I am 18 years old. I am a student of class 2.";
+            Student student = new Student("Tom", 18);
+            Class @class = new Class(2);
+
+            //when
+            //check introduction before enrollment
+            Assert.Equal("My name is Tom. I am 18 years old. I am a student.", student.Introduce());
+
+            student.Enroll(@class);
+            string actual = student.Introduce();
+
+            //then
+            Assert.Equal(expected, actual);
+        }
     }
 }
