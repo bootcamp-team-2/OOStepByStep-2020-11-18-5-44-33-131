cd392eabaee8393b7906e9f08285dcf240840595
Lin Liu <LLiu41@slb.com>
2020-11-18 17:26:05 +0800

    feat: create Person class that can introduce

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Introduce()
+        {
+            return $"My name is {name}. I am {age} years old.";
+        }
+    }
+}
---
+++ OOStepByStepTest/PersonTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using OOStepByStep;
+
+namespace OOStepByStepTest
+{
+    public class PersonTest
+    {
+        [Fact]
+        public void Should_Return_Message_With_Name_And_Age_When_Introduce()
+        {
+            //given
+            string expected = "My name is Tom. I am 21 years old.";
+            Person person = new Person("Tom", 21);
+
+            //when
+            string actual = person.Introduce();
+
+            //then
+            Assert.Equal(expected, actual);
+        }
+    }
+}
